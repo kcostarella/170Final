0017a08578bcba8394afa8ac1ddfda6d47291636
JonathanNakhla <jonathan.g.nakhla@gmail.com>
2015-04-29 11:18:12 -0700

    Annotated the parser code.

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ T = 1 # number of test cases
 fout = open ("answer.out", "w")
 for t in xrange(1, T+1):
     fin = open(str(t) + ".in", "r")
-    N = int(fin.readline())
+    N = int(fin.readline()) # N is the number of cities
+    print(N)
     d = [[] for i in range(N)] #c reates an array of N empty arrays
     for i in xrange(N):
         d[i] = [int(x) for x in fin.readline().split()] # fills up the all N arrarys in array d
