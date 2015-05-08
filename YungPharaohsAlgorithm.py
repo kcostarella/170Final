6178461ea9d909b721e5478a50c46c7aeeaf912d
Ko Costarella <kcostarella@koko.local>
2015-05-07 20:52:07 -0700

    shift tab

--- YungPharaohsAlgorithm.py
+++ YungPharaohsAlgorithm.py
@@ -30,60 +30,59 @@ for t in xrange(1, T+1):
 
 
 
-    def GenerateSolutions(iSeed,uSeed):
-    	"""Generates a dictionary of <solution,weight> pairs, where sollution is a solution to an instance of NTSP
-    	and the weight value is the weight or cost associated with the given solution. iSeed is an integer that represents
-    	the number of solution keys returned in the allSolutions dictionary. uSeed is another integer that represents 
-    	how many iterations of updateSolution will be called per solution"""
-    	allSolutions = {}
-    	for i in range(0,iSeed):
-    		solution,weight = GenerateSolution() #generates a random instance
-    		for i in range(0,uSeed):
-    			solution, weight = UpdateSolution(solution,weight) #hill climbing improvement algorithm
-    		allSolutions[solution] = weight #adds the solution,weight pair to the allSolutions dict
-    	return allSolutions
+def GenerateSolutions(iSeed,uSeed):
+	"""Generates a dictionary of <solution,weight> pairs, where sollution is a solution to an instance of NTSP
+	and the weight value is the weight or cost associated with the given solution. iSeed is an integer that represents
+	the number of solution keys returned in the allSolutions dictionary. uSeed is another integer that represents 
+	how many iterations of updateSolution will be called per solution"""
+	allSolutions = {}
+	for i in range(0,iSeed):
+		solution,weight = GenerateSolution() #generates a random instance
+		for i in range(0,uSeed):
+			solution, weight = UpdateSolution(solution,weight) #hill climbing improvement algorithm
+		allSolutions[solution] = weight #adds the solution,weight pair to the allSolutions dict
+	return allSolutions
 
-    def GenerateSolution():
-   		"""Generates a completely random solution for the particular instance of NTSP"""
-   		solution = []
-    	totalWeight = 0 # the total weight of our answer
-    	hasVisited = [False] * N
-    	colorCount = [] #Color count is a 2 element array where colorCount[0] = currentColor and colorCount[1] = the number of times 
-    	#We've seen the currentColor
+def GenerateSolution():
+		"""Generates a completely random solution for the particular instance of NTSP"""
+		solution = []
+	totalWeight = 0 # the total weight of our answer
+	hasVisited = [False] * N
+	colorCount = [] #Color count is a 2 element array where colorCount[0] = currentColor and colorCount[1] = the number of times 
+	#We've seen the currentColor
 
-		#Initilize (Pick a starting State)
-		currentState = random.randint(0,N-1) #picks a random start position
-		colorCount[0] = c[currentState] #Sets colorCount[0] to the color of the currentState...
-		colorCount[1] = 1 #... and We've now seen this color once...
-		hasVisited[currentState] = True #... and We've just visited this state...
-		solution.append[currentState] #... and this state is part of our solution
-		#Travel (Loops through the remaning N-1 cities)    	
-    	for n in range(0,N-1):
-    		prevState = currentState #saves the previous state
-    		validMoves = GenerateValidPositions(hasVisted) #generates valid next moves determined by internal logic in GenerateValidPositions
-    		currentState = validMoves[random.randint(0,len(validMoves)-1)] #assigns the currentState to a random state from the validMoves
+	#Initilize (Pick a starting State)
+	currentState = random.randint(0,N-1) #picks a random start position
+	colorCount[0] = c[currentState] #Sets colorCount[0] to the color of the currentState...
+	colorCount[1] = 1 #... and We've now seen this color once...
+	hasVisited[currentState] = True #... and We've just visited this state...
+	solution.append[currentState] #... and this state is part of our solution
+	#Travel (Loops through the remaning N-1 cities)    	
+	for n in range(0,N-1):
+		prevState = currentState #saves the previous state
+		validMoves = GenerateValidPositions(hasVisted) #generates valid next moves determined by internal logic in GenerateValidPositions
+		currentState = validMoves[random.randint(0,len(validMoves)-1)] #assigns the currentState to a random state from the validMoves
 
-    		totalWeight += d[prevState][currentState] #increments the total weight by the cost to travel form prevState to currentState
-    		#updates the colorCount depending on the color of currentState
-    		if colorCount[0] == c[currentState]:
-    			colorCount[0] += 1
-    		else:
-    			colorCount[0] = c[currentState]
-    			colorCount[0] = 1
-    		hasVisited[currentState] = True #Visit the current state#
-    		solution.append[currentState] #Add it to our solution#
-    	return (solution,totalWeight) #Return the solution and the totalWeight associated with it
+		totalWeight += d[prevState][currentState] #increments the total weight by the cost to travel form prevState to currentState
+		#updates the colorCount depending on the color of currentState
+		if colorCount[0] == c[currentState]:
+			colorCount[0] += 1
+		else:
+			colorCount[0] = c[currentState]
+			colorCount[0] = 1
+		hasVisited[currentState] = True #Visit the current state#
+		solution.append[currentState] #Add it to our solution#
+	return (solution,totalWeight) #Return the solution and the totalWeight associated with it
 
-    def GenerateValidPositions(visited, colorCount):
-    	""" returns an array containing the indices of valid states to visit, where when
-    	visited[i] = False indicates that index i is a valid state to visit. """
-    	validMoves = []
-    	for state in range(0,visited):
-    		if (!visited[state] and (colorCount[1] < 3 or colorCount[0] != c[state])) #A state is valid if it hasnt been visited yet AND either its only seen the current color 
-				#once or twice OR the color of the next state is different than the current color
-				validMoves.append(state)
-		return validMoves
+def GenerateValidPositions(visited, colorCount):
+	""" returns an array containing the indices of valid states to visit, where when
+	visited[i] = False indicates that index i is a valid state to visit. """
+	validMoves = []
+	for state in range(0,visited):
+		if (!visited[state] and (colorCount[1] < 3 or colorCount[0] != c[state])) #A state is valid if it hasnt been visited yet AND either its only seen the current color 
+			#once or twice OR the color of the next state is different than the current color
+			validMoves.append(state)
+	return validMoves
 
-    def UpdateSolution(instance, weight):
-    	return (instance, weight)
-
+def UpdateSolution(instance, weight):
+	return (instance, weight)
